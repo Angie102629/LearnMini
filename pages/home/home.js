fced8f378d8ad04b738ddb79ebb502207b9e977b
Angie102629 <990859041@qq.com>
2020-03-12 07:20:05 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,85 @@
 // pages/home/home.js
+// getApp()获取App()产生的示例对象
+const app = getApp()
+console.log(app.globalData.name)
+console.log(app.globalData.age)
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // -----------2.初始化数据-------
+  data:{
+    message:'哈哈',
+    list:[]
+  },
+  // -----------1.监听页面的生命周期函数-------
+  // 页面被加载出来
+  onLoad() {
+    console.log('onLoad')
+    const _this = this
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   // success:(res) => {
+    //   //   console.log(res)
+    //   //   const data = res.data.data.list;
+    //   //   this.setData({
+    //   //     list:data
+    //   //   })
+    //   // }
+    //   success:function(res) {
+    //     console.log(res)
+    //     const data = res.data.data.list;
+    //     _this.setData({
+    //       list:data
+    //     })
+    //   }
+    // })
+  },
+  
+  //  -----------3.监听wxml中相关的一些事件-------
+  handleGetUserInfo(event) {
+    console.log(event)
+  },
+  bandleViewClic() {
+    console.log('哈哈被点击了')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //  -----------4.监听wxml中相关的一些事件-------
+  // 监听页面的滚动
+  onPagescroll(obj) {
+    console.log(obj)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onReachBottom(){
+    console.log('页面滚动到顶部')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPullDownRefresh(){
+    console.log("下拉刷新的事件")
   }
+  
+  // data:{
+  //   name:'Coderwhy',
+  //   age: 18,
+  //   students:[
+  //     {id: 110, name: 'kode', age:30},
+  //     {id: 111, name: 'james', age: 30 },
+  //     {id: 112, name: 'curry', age: 30 },
+  //     {id: 113, name: 'why', age: 30 }
+  //   ],
+  //   counter:0
+  // },
+
+  // handleBtnClick() {
+  //   this.setData({
+  //     // 直接绑定100
+  //     // counter:100
+  //     // 递增
+  //     counter: this.data.counter +1      
+  //   })      
+  // },
+
+  // handleSubtraction() {
+  //   this.setData({
+  //     counter: this.data.counter - 1
+  //   })
+  // }
+ 
+  
 })
